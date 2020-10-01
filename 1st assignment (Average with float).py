bff1abd8f138e2cef7516f3351d03dd974832b4f
Justyce Graham <71726130+LilGramz@users.noreply.github.com>
2020-09-30 23:50:08 -0400

    with repeat

--- 1st assignment (Average with float).py
+++ 1st assignment (Average with float).py
@@ -7,10 +7,19 @@ scorecount = 0
 numberOfTests = int(input("please enter the number of tests you want to average: "))
 
 #Make this stuff repeat untill scorecount = numberOfTests
-score = int(input("please enter a score: "))
-scorecount = scorecount + 1
-total = total + score
+while scorecount < numberOfTests:
+    score = int(input("please enter a score: "))
 
+    #Scorecount is 0 bu increases by 1 each time a number is imputed by user
+    scorecount = scorecount + 1
+
+    #total is zero but increases each time  anumber is imputed by the user
+    total += score
+
+    #when the number of scores= the initial number imputed the loop ends
+    if scorecount == numberOftest:
+        break
+    
 average = total/scorecount
 print("the average is ",average)
 
